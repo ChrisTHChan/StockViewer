e625f64966061a14a01e6e66f441239cad562924
mrchrischan <chanchris1117@gmail.com>
2019-07-08 17:38:44 -0400

    added portfolio feature.

--- src/App.js
+++ src/App.js
@@ -4,6 +4,14 @@ import InputField from './InputField';
 import StockInformation from './StockInformation'
 import loading from './loading.gif'
 
+//split bottom screen into 2 vertical halves (html/css) CHECK
+//the left side will be your portfolio. CHECK
+//add input field when you retrieve a stock from the stock list to add notes about the stock when its added to portfolio. CHECK
+//add button when you retrieve a stock from the stock list to add it to your portfolio. CHECK 
+//add functionality to buttons, wipe text from text fields when button is clicked. CHECK
+//add a method to allow you to search through your portfolio for various stocks that you want to see.
+//perhaps add a textfield that allows you to choose how many of one stock you want to add, and the total price, show percentage of portfolio that is made up from that stock.
+
 class App extends Component {
   constructor() {
     super()
@@ -16,8 +24,9 @@ class App extends Component {
       stockList: [],
       statistics: [],
       input: '',
+      notesInput: '',
       userStockSelection: '',
-      symbol: '.',
+      symbol: '',
       currentPrice: '',
       nominalChange: '',
       percentChange: '',
@@ -26,11 +35,24 @@ class App extends Component {
       dailyHigh: '',
       dailyLow: '',
       stockVolume: '',
+      portfolioList: []
     }
   }
 
   onSearchChange = (e) => {
     this.setState({input: e.target.value.toUpperCase()})
+  }
+
+  onNotesChange = (e) => {
+    this.setState({notesInput: e.target.value})
+  }
+
+  onAppendStock = () => {
+    document.querySelector('.notesinput').value = ''
+    const list = this.state.portfolioList
+    list.push(`${this.state.statistics['01. symbol']} (${this.state.notesInput})`)
+    this.setState({portfolioList: list})
+    this.setState({showInfo: false})
   }
 
   showBadSearchMessage = () => {
@@ -46,8 +68,14 @@ class App extends Component {
       this.getStocksFunction()
     }
   }
+  onPressEnterNotesChange = (e) => {
+    if (e.which === 13) {
+      this.onAppendStock()
+    }
+  }
 
   getStocksFunction = () => {
+    document.querySelector('.searchBar').value = ''
     this.setState({showInfo: false})
     this.setState({apiPromiseRejection: false})
     if (this.state.input === '') {
@@ -70,6 +98,7 @@ class App extends Component {
       }))
       .catch((error) => {
         this.setState({apiPromiseRejection: true})
+        console.log(error)
       })
     }
   }
@@ -96,6 +125,7 @@ class App extends Component {
       }))
       .catch((error) => {
         this.setState({apiPromiseRejection: true})
+        console.log(error)
       })
     })
   }
@@ -103,45 +133,66 @@ class App extends Component {
   render() {
     if (this.state.apiPromiseRejection === false) {
       return (
-        
         <div className="App">
           <h1>Welcome to StockViewer</h1>
-          <h2>Enter a ticker to search for stocks!</h2>
+          <h2>Search for stocks and put them into your portfolio!</h2>
           <div className="center">
             <InputField searchChange={this.onSearchChange} enterPress={this.onEnterPress}/>
             <button onClick={this.getStocksFunction}>Search!</button>
           </div><br/>
-          <h1 style={{display: this.state.badSearch === true ? 'block' : 'none'}}>No Results Found.</h1>
-          <img src={loading} alt='' className="loading" style={{display: this.state.apiLoading === true ? 'block' : 'none'}}/>
-          <div style={{display: this.state.showStockList === true ? 'block' : 'none'}}>
-            {this.state.stockList.map((stock, i) => {
-              return <h3 onClick={this.getInfoFunction} id={i} key={i}>{stock['1. symbol']}: {stock['2. name']}</h3>
-            })}
+          <div className='twoscreencontainer'>
+            <div className='portfolio'>
+              <h1>Portfolio:</h1>
+              <div>
+                {this.state.portfolioList.map((stock, i) => {
+                  return <p key={i}>{`${i + 1}. `}{stock}</p>
+                })}
+              </div>
+            </div>
+            <div className='info'>
+              <h1 style={{display: this.state.badSearch === true ? 'block' : 'none'}}>No Results Found.</h1>
+              <img src={loading} alt='' className="loading" style={{display: this.state.apiLoading === true ? 'block' : 'none'}}/>
+              <div style={{display: this.state.showStockList === true ? 'block' : 'none'}}>
+                {this.state.stockList.map((stock, i) => {
+                  return <h3 onClick={this.getInfoFunction} id={i} key={i}>{stock['1. symbol']}: {stock['2. name']}</h3>
+                })}
+              </div>
+              <StockInformation 
+                        symbol={this.state.symbol}
+                        currentPrice={this.state.currentPrice}
+                        nominalChange={this.state.nominalChange}
+                        percentChange={this.state.percentChange} 
+                        dailyOpen={this.state.dailyOpen}
+                        dailyClose={this.state.dailyClose}
+                        dailyHigh={this.state.dailyHigh}
+                        dailyLow={this.state.dailyLow}
+                        stockVolume={this.state.stockVolume}
+                        show={this.state.showInfo}
+                        error={this.state.badSearch}
+                        notesChange={this.onNotesChange}
+                        pressEnterNotesChange={this.onPressEnterNotesChange}
+                        appendStock={this.onAppendStock}/>
+            </div>
           </div>
-          <StockInformation 
-                    symbol={this.state.symbol}
-                    currentPrice={this.state.currentPrice}
-                    nominalChange={this.state.nominalChange}
-                    percentChange={this.state.percentChange} 
-                    dailyOpen={this.state.dailyOpen}
-                    dailyClose={this.state.dailyClose}
-                    dailyHigh={this.state.dailyHigh}
-                    dailyLow={this.state.dailyLow}
-                    stockVolume={this.state.stockVolume}
-                    show={this.state.showInfo}
-                    error={this.state.badSearch}/>
         </div>
       )
-    } if (this.state.apiPromiseRejection === true) {
+    } else if (this.state.apiPromiseRejection === true) {
       return (
         <div className="App">
           <h1>Welcome to StockViewer</h1>
-          <h2>Enter a ticker to search for stocks!</h2>
+          <h2>Search for stocks and put them into your portfolio!</h2>
           <div className="center">
             <InputField searchChange={this.onSearchChange} enterPress={this.onEnterPress}/>
             <button onClick={this.getStocksFunction}>Search!</button>
           </div><br/>
-          <h1>Failed to retrieve data! Check your internet, or try again later!</h1>
+          <div className='twoscreencontainer'>
+            <div className='portfolio'>
+              <h1>Portfolio:</h1>
+            </div>
+            <div className='info'>
+              <h1>Failed to retrieve data! Check your internet, or try again later!</h1>
+            </div>
+          </div>
         </div>
       )
     }
--- src/StockInformation.js
+++ src/StockInformation.js
@@ -3,7 +3,7 @@ import React from 'react';
 const StockInformation = (props) => {
         return (
             <div style={{display: props.show === true ? 'block' : 'none'}}>
-                <h1>Stock Symbol: {props.symbol}</h1>
+                <h4>Stock Symbol: {props.symbol}</h4>
                 <h4>Current Price: {props.currentPrice}</h4>
                 <h4>Last Day's Close: {props.dailyClose}</h4>
                 <h4>Nominal Change: {props.nominalChange}</h4>
@@ -12,8 +12,10 @@ const StockInformation = (props) => {
                 <h4>Today's High: {props.dailyHigh}</h4>
                 <h4>Today's Low: {props.dailyLow}</h4>
                 <h4>Stock Volume: {props.stockVolume}</h4>
+                <h5>Notes:<input placeholder='Enter any notes here' className='notesinput' onChange={props.notesChange} onKeyPress={props.pressEnterNotesChange}></input></h5>
+                <button onClick={props.appendStock}>Submit to Portfolio!</button>
             </div>
         )
 }
 
-export default StockInformation
+export default StockInformation;
