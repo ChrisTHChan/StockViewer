bc37d78c96d16e2198682619eddee31912561080
mrchrischan <chanchris1117@gmail.com>
2019-07-22 16:03:47 -0400

    Updates

---
+++ precache-manifest.c862867984c48f3524fa64271dfc634b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffd207c1d3cb075e624c2a68c9ecf090",
+    "url": "/StockViewer/index.html"
+  },
+  {
+    "revision": "1333e72e84edbba8a93c",
+    "url": "/StockViewer/static/css/main.af556a1b.chunk.css"
+  },
+  {
+    "revision": "43d1f55c2a696b4563b8",
+    "url": "/StockViewer/static/js/2.179f4c70.chunk.js"
+  },
+  {
+    "revision": "1333e72e84edbba8a93c",
+    "url": "/StockViewer/static/js/main.1e6a7fbb.chunk.js"
+  },
+  {
+    "revision": "35e60b2908fcd7277977",
+    "url": "/StockViewer/static/js/runtime~main.b053cead.js"
+  },
+  {
+    "revision": "c6183db629a1c3ce00a49adc040070cc",
+    "url": "/StockViewer/static/media/header.c6183db6.jpg"
+  },
+  {
+    "revision": "67f2b4f6521d1a09f6611568cacad962",
+    "url": "/StockViewer/static/media/scrolldown.67f2b4f6.svg"
+  }
+]);
